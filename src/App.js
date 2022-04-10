56ef0bdd5067a4a30044efc9bffceeb3c2496c87
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-04-10 15:08:36 +0530

    cmmt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const brand=["Samsung","HTC","Micromax","Apple"];
+const oprt = ["Android", "Blckberry", "iPhone", "WindowsPhone"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <div>
+     <h1>Mobile Operating System</h1>
+     <ul>
+       {
+         brand.map((e) => {
+           return <Manu menu={e}/>
+         })
+       }
+     </ul>
+   </div>
+   <div>
+     <h1>Mobile Manufacturers</h1>
+     <ul>
+     {
+         oprt.map((e) => {
+           return <Manu menu={e}/>
+         })
+       }
+     </ul>
+   </div>
+   </div>
+  
   );
+}
+
+
+function Manu({menu}) {
+    return <li>{menu}</li>
 }
 
 export default App;
